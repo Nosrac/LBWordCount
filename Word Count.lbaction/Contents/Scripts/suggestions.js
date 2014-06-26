6f27c9fc5bba32377574359562d1ddf4e4abdbab
Kyle Carson <elyknosrac@gmail.com>
2014-06-25 22:58:10 -0500

    Show lines text when necessary

--- Word Count.lbaction/Contents/Scripts/suggestions.js
+++ Word Count.lbaction/Contents/Scripts/suggestions.js
@@ -1,13 +1,22 @@
 function wordcount(string)
 {
 	var words = string.split(/\s+/).length;
-	return [
+	var lines = string.split(/\n/).length;
+
+	var results = [
 		{
 			title: words + " Words",
 		}, {
 			title: string.length + " Characters",
 		}
 	];
+
+	if (lines > 1)
+	{
+		results.push({ title: lines + " Lines" });
+	}
+
+	return results;
 }
 
 function runWithString(string)
